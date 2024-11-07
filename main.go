6ecb58166e3629f6c26e65987559f2f2e0e56c1c
Kevin Mollenhauer <kevin.mollenhauer@iktos.com>
2024-11-07 14:06:42 +0100

    Missing ctrl+s at the end of the interview

--- main.go
+++ main.go
@@ -13,13 +13,37 @@ type Downtime struct {
 
 func main() {
 	fmt.Println(len(os.Args), os.Args)
-
+	// Check app name is present
 	if len(os.Args) < 2 {
 		fmt.Println("Too few arguments. Need at least app name")
 		return
 	}
 
+	// Set downtime on app
+	err := datadogDowntime(os.Args[1], true)
+	if err != nil {
+		fmt.Println("Unable to set downtime on app")
+		return
+	}
 
+	// Launch ArgiCD application sync
+	err = argoCDSyncApp(os.Args[1])
+	if err != nil {
+		fmt.Println("Unable to launch ArgoCD application sync")
+	}
+
+	// Remove downtime anyway
+	err = datadogDowntime(os.Args[1], false)
+	if err != nil {
+		fmt.Println("Unable to remove downtime on app")
+		fmt.Println("Warning, downtime is still active")
+		// Fallback can be to use schedule end for downtime
+		// like maxDowntimeSeconds := 3600
+	}
+
+
+	// Remove downtime on app
+	datadogDowntime(os.Args[1], false)
 }
 
 func datadogDowntime(app string, active bool) (error) {
@@ -46,11 +70,15 @@ func datadogDowntime(app string, active bool) (error) {
 
 	// Check if app if already down
 	for _, e range Downtime {
-
+				// if already in proper state
+				// return nil
+				// else set downtime on app
 	}
-
-		// if already in proper state
-		return nil
-
-	// if not 
+	return nil
 } 
+
+func argoCDSyncApp(app string) (err) {
+	// POST https://cd.apps.argoproj.io/api/v1/applications/{name}/sync
+	// if return 200 {return nil}
+	// else {return err}
+}
